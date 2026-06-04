ce35c996de172091cba96b3a774085fcd756191f
Roshan Iruku <roshaniruku@gmail.com>
2026-06-04 01:15:39 -0400

    added

---
+++ sketchybar/settings.lua
@@ -0,0 +1,18 @@
+return {
+  paddings = 3,
+  group_paddings = 5,
+
+  icons = "NerdFont",
+
+  font = {
+    text = "JetBrainsMono Nerd Font",
+    numbers = "JetBrainsMono Nerd Font",
+    style_map = {
+      ["Regular"] = "Regular",
+      ["Semibold"] = "Medium",
+      ["Bold"] = "SemiBold",
+      ["Heavy"] = "Bold",
+      ["Black"] = "ExtraBold",
+    },
+  },
+}
