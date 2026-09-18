2142d9f40cbc6db68145d9505169089f6fe0f9df
Roshan Iruku <roshaniruku@gmail.com>
2026-09-18 03:12:35 -0400

    nvim: auto-install clangd for C/C++ LSP

--- nvim/.config/nvim/lua/anivim/plugins/lsp.lua
+++ nvim/.config/nvim/lua/anivim/plugins/lsp.lua
@@ -17,7 +17,7 @@ return {
 		config = function()
 			require("mason").setup()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" },
+				ensure_installed = { "lua_ls", "clangd" },
 			})
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
